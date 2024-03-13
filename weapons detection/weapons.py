09570ea97c4967756ad08b6e55e7739ab4134e32
Aditya Ningule <adiningule2@gmail.com>
2024-03-13 19:58:03 +0530

    weapons detection

---
+++ weapons detection/weapons.py
@@ -0,0 +1,124 @@
+import cv2
+from ultralytics import YOLO
+
+# def detect_objects_in_photo(image_path):
+#     image_orig = cv2.imread(image_path)
+    
+#     yolo_model = YOLO('./runs/detect/Normal_Compressed/weights/best.pt')
+    
+#     results = yolo_model(image_orig)
+
+#     for result in results:
+#         classes = result.names
+#         cls = result.boxes.cls
+#         conf = result.boxes.conf
+#         detections = result.boxes.xyxy
+
+#         for pos, detection in enumerate(detections):
+#             if conf[pos] >= 0.5:
+#                 xmin, ymin, xmax, ymax = detection
+#                 label = f"{classes[int(cls[pos])]} {conf[pos]:.2f}" 
+#                 color = (0, int(cls[pos]), 255)
+#                 cv2.rectangle(image_orig, (int(xmin), int(ymin)), (int(xmax), int(ymax)), color, 2)
+#                 cv2.putText(image_orig, label, (int(xmin), int(ymin) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1, cv2.LINE_AA)
+
+#     result_path = "./imgs/Test/teste.jpg"
+#     cv2.imwrite(result_path, image_orig)
+#     return result_path
+
+# def detect_objects_in_video(video_path):
+#     yolo_model = YOLO('./runs/detect/Normal_Compressed/weights/best.pt')
+#     video_capture = cv2.VideoCapture(video_path)
+#     width = int(video_capture.get(3))
+#     height = int(video_capture.get(4))
+#     fourcc = cv2.VideoWriter_fourcc(*'XVID')
+#     result_video_path = "detected_objects_video2.avi"
+#     out = cv2.VideoWriter(result_video_path, fourcc, 20.0, (width, height))
+
+#     while True:
+#         ret, frame = video_capture.read()
+#         if not ret:
+#             break
+#         results = yolo_model(frame)
+
+#         for result in results:
+#             classes = result.names
+#             cls = result.boxes.cls
+#             conf = result.boxes.conf
+#             detections = result.boxes.xyxy
+
+#             for pos, detection in enumerate(detections):
+#                 if conf[pos] >= 0.5:
+#                     xmin, ymin, xmax, ymax = detection
+#                     label = f"{classes[int(cls[pos])]} {conf[pos]:.2f}" 
+#                     color = (0, int(cls[pos]), 255)
+#                     cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), color, 2)
+#                     cv2.putText(frame, label, (int(xmin), int(ymin) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1, cv2.LINE_AA)
+
+#         out.write(frame)
+#     video_capture.release()
+#     out.release()
+
+#     return result_video_path
+
+# def detect_objects_and_plot(path_orig):
+#     image_orig = cv2.imread(path_orig)
+    
+#     yolo_model = YOLO('./runs/detect/Normal_Compressed/weights/best.pt')
+    
+#     results = yolo_model(image_orig)
+
+#     for result in results:
+#         classes = result.names
+#         cls = result.boxes.cls
+#         conf = result.boxes.conf
+#         detections = result.boxes.xyxy
+
+#         for pos, detection in enumerate(detections):
+#             if conf[pos] >= 0.5:
+#                 xmin, ymin, xmax, ymax = detection
+#                 label = f"{classes[int(cls[pos])]} {conf[pos]:.2f}" 
+#                 color = (0, int(cls[pos]), 255)
+#                 cv2.rectangle(image_orig, (int(xmin), int(ymin)), (int(xmax), int(ymax)), color, 2)
+#                 cv2.putText(image_orig, label, (int(xmin), int(ymin) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1, cv2.LINE_AA)
+    
+#     cv2.imshow("Teste", image_orig)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+
+
+def detect_objects_in_realtime():
+    yolo_model = YOLO('./best.pt')
+    video_capture = cv2.VideoCapture(0)  # Open the default camera (usually the webcam)
+    
+    while True:
+        ret, frame = video_capture.read()
+        if not ret:
+            break
+        results = yolo_model(frame)
+
+        for result in results:
+            classes = result.names
+            cls = result.boxes.cls
+            conf = result.boxes.conf
+            detections = result.boxes.xyxy
+
+            for pos, detection in enumerate(detections):
+                if conf[pos] >= 0.5:
+                    xmin, ymin, xmax, ymax = detection
+                    label = f"{classes[int(cls[pos])]} {conf[pos]:.2f}" 
+                    color = (0, int(cls[pos]), 255)
+                    cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), color, 2)
+                    cv2.putText(frame, label, (int(xmin), int(ymin) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1, cv2.LINE_AA)
+
+        cv2.imshow('Real-time Object Detection', frame)
+        
+        # Press 'q' to exit the loop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+# Call the function to start real-time object detection
+detect_objects_in_realtime()
